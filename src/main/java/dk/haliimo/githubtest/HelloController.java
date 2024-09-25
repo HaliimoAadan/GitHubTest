a4cbbfe5cc164fc747c446dd087f18b7ff178bed
HaliimoAadan <haliimasheikh@hotmail.com>
2024-09-25 10:49:16 +0200

    Added a new file

--- src/main/java/dk/haliimo/githubtest/HelloController.java
+++ src/main/java/dk/haliimo/githubtest/HelloController.java
@@ -6,9 +6,11 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private Label goodbyeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        goodbyeText.setText("Goodbye!");
     }
 }
